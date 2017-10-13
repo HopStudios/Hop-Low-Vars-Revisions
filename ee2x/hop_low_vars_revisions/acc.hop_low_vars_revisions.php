418e8dd5382ca9d4a6fb7a78034a740374c2076d
Louis Dekeister <louis@hopstudios.com>
2017-10-13 10:13:37 -0700

    add logic to load our script file into the accessory when showing a Low Variable page

--- ee2x/hop_low_vars_revisions/acc.hop_low_vars_revisions.php
+++ ee2x/hop_low_vars_revisions/acc.hop_low_vars_revisions.php
@@ -5,34 +5,43 @@ require_once PATH_THIRD.'hop_low_vars_revisions/_config.php';
 
 class Hop_low_vars_revisions_acc {
 
-    public $name            = 'Low Variable Revision Tracker';
-    public $id              = HOP_LOW_VARS_REVISIONS_SHORT_NAME;
-    public $version         = HOP_LOW_VARS_REVISIONS_VERSION;
-    public $description     = 'Accessory for Low Variables Revision Tracker';
-    public $sections        = array();
+	public $name            = 'Low Variable Revision Tracker';
+	public $id              = HOP_LOW_VARS_REVISIONS_SHORT_NAME;
+	public $version         = HOP_LOW_VARS_REVISIONS_VERSION;
+	public $description     = 'Accessory for Low Variables Revision Tracker';
+	public $sections        = array();
 
-    function __construct()
-    {
+	function __construct()
+	{
 
-        //Set differently if we are on a publish or edit page
-        if(         ee()->input->get('D') == "cp" && ee()->input->get('module') == 'low_variables') {
-            
-            }
-		else {
+		// Set differently if we are on a publish or edit page
+		if(ee()->input->get('D') == "cp" && ee()->input->get('module') == 'low_variables') {
+
+		} else {
 			return false;
-			}
+		}
 
-        //Hide the tab on all pages
-        $this->sections[] = '<script type="text/javascript" charset="utf-8">$("#accessoryTabs a.'.$this->id.'").parent().remove();</script>';
-    }
+		// Hide the tab on all pages
+		$this->sections[] = '<script type="text/javascript" charset="utf-8">$("#accessoryTabs a.'.$this->id.'").parent().remove();</script>';
 
-    /**
-     * Set Sections
-     * This function must exist, but is empty
-     */
-    public function set_sections() {
+		// Add a var with CP url to fetch revisions as JSON
+		$addon_cp_url = str_replace('&amp;', '&', BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module='.HOP_LOW_VARS_REVISIONS_SHORT_NAME);
+		$load_revisions_endpoint = $addon_cp_url.'&method=loadRevisions';
+		$show_revision_endpoint = $addon_cp_url.'&method=showRevision';
+		$this->sections[] = '<script type="text/javascript" charset="utf-8"> var hlvrLoadUrl = "'.$load_revisions_endpoint.'"; var hlvrShowUrl = "'.$show_revision_endpoint.'"; </script>';
 
-        } // end public function set_sections()
+		// Add our main script to load revisions list
+		$script = file_get_contents(PATH_THIRD.'hop_low_vars_revisions/assets/low_var_revisions.js');
+		$this->sections[] = '<script type="text/javascript" charset="utf-8">'.$script.'</script>';
+	}
+
+	/**
+	 * Set Sections
+	 * This function must exist, but is empty
+	 */
+	public function set_sections() {
+
+	}
 
 
 }
